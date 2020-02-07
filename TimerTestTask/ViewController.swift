60c6128edc95342e47cf317596e7b44552756c83
Oleksandra Myshkina <myshkinasasha@gmail.com>
2020-02-07 20:21:33 +0200

    Initial Commit

---
+++ TimerTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerTestTask
+//
+//  Created by Sasha Myshkina on 07.02.2020.
+//  Copyright © 2020 Sasha Myshkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
